a819b845183c53f33f7b58fa3560f065789852f9
George Araujo <george.gcac@gmail.com>
2023-07-27 13:46:35 -0300

    Updated aaai spider
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- papers_scrapper/spiders/aaai.py
+++ papers_scrapper/spiders/aaai.py
@@ -1,3 +1,4 @@
+import re
 from os import path
 
 import scrapy
@@ -10,140 +11,67 @@ from ..items import PdfFilesItem
 class AAAISpider(BaseSpider):
     name = 'aaai'
     allowed_domains = ['aaai.org/', 'ojs.aaai.org/']
-    start_urls = ['https://aaai.org/Library/AAAI/']
+    start_urls = ['https://aaai.org/aaai-publications/aaai-conference-proceedings/']
 
     def __init__(self, conference: str = '', year: str = '', new_style: bool = True, subpage: bool = True):
         BaseSpider.__init__(self, conference, year)
-
-        self.new_style = new_style
-        if isinstance(new_style, str):
-            new_style = new_style.lower()
-            if new_style == 'false' or new_style == '0':
-                self.new_style = False
-            else:
-                self.new_style = True
-        self.subpage = subpage
-        if isinstance(subpage, str):
-            subpage = subpage.lower()
-            if subpage == 'false' or subpage == '0':
-                self.subpage = False
-            else:
-                self.subpage = True
+        self.regex = re.compile(rf'{conference}-[\d]+-{year}')
 
     def start_requests(self):
         for url in self.start_urls:
-            # aaai18contents.php
-            conference_url = f'{url}{self.conference}{self.year[2:]}contents.php'
-            self.logger.info(
-                f'Start scraping {conference_url} for {self.year}')
-            yield scrapy.Request(url=conference_url, callback=self.parse)
+            self.logger.info(f'Start scraping {url} for {self.year}')
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response: scrapy.http.TextResponse):
         # inspect_response(response, self)
-        if self.subpage:
-            for link in response.xpath('//*[@id="box6"]/div/ul/li/a/@href'):
-                subpage_url = response.urljoin(link.get())
-                self.logger.debug(f'Found {subpage_url}')
+        # https://aaai.org/proceeding/aaai-36-2022/
+        for link in response.xpath('//a[@href]'):
+            link_str = link.xpath('@href').get()
+            if self.regex.search(link_str) is not None:
                 yield scrapy.Request(
-                    url=subpage_url,
-                    callback=self.parse_subpage,
-                    dont_filter=True
-                )
-        else:
-            papers_links = response.xpath(
-                '//*[@id="box6"]/div/p/a[1]')
-
-            for paper_link in papers_links:
-                title = paper_link.xpath('text()').get()
-                if 'this proceedings is also available in' not in title.lower():
-                    abstract_link = paper_link.xpath('@href').get()
-                    item = PdfFilesItem()
-                    item['abstract_url'] = abstract_link.split('/')[-1]
-                    item['title'] = title
-
-                    if not self.new_style:
-                        splitted_link = abstract_link.split('/')
-                        if splitted_link[-2] == 'view':
-                            splitted_link[-2] = 'viewPaper'
-                            abstract_link = '/'.join(splitted_link)
-
-                    self.logger.debug(f'Found {abstract_link} for {title}')
-                    yield scrapy.Request(
-                        url=abstract_link,
-                        callback=self.parse_abstract,
-                        dont_filter=True,
-                        meta={'item': item}
-                    )
-
-    def parse_subpage(self, response: scrapy.http.TextResponse):
-        # inspect_response(response, self)
-        papers_links = response.xpath(
-            '//*[@id="box6"]/div/p/a[1]')
-
-        for paper_link in papers_links:
-            title = paper_link.xpath('text()').get()
-            if 'this proceedings is also available in' not in title.lower():
-                abstract_link = paper_link.xpath('@href').get()
-                item = PdfFilesItem()
-                item['abstract_url'] = abstract_link.split('/')[-1]
-                item['title'] = title
-
-                self.logger.debug(f'Found {abstract_link} for {title}')
-                yield scrapy.Request(
-                    url=abstract_link,
-                    callback=self.parse_abstract,
+                    url=link_str,
+                    callback=self.parse_proceedings,
                     dont_filter=True,
-                    meta={'item': item}
                 )
+
+    def parse_proceedings(self, response: scrapy.http.TextResponse):
+        # inspect_response(response, self)
+        for link in response.xpath('//*[@id="genesis-content"]/ul/li/a/@href').getall():
+            yield scrapy.Request(
+                url=link,
+                callback=self.parse_papers,
+                dont_filter=True,
+            )
+
+    def parse_papers(self, response: scrapy.http.TextResponse):
+        # inspect_response(response, self)
+        links = response.xpath('//*[@id="genesis-content"]/div/ul/li/h5/a')
+        authors_line = response.xpath('//*[@id="genesis-content"]/div/ul/li/span/p[1]/text()').getall()
+
+        for link, authors in zip(links, authors_line):
+            title = link.xpath('text()').get().strip()
+            abstract_link = link.xpath('@href').get()
+
+            item = PdfFilesItem()
+            item['abstract_url'] = abstract_link.split('/')[-2]
+            item['title'] = title
+            item['authors'] = authors.strip()
+
+            self.logger.debug(f'Found {abstract_link} for {title}')
+            yield scrapy.Request(
+                url=abstract_link,
+                callback=self.parse_abstract,
+                dont_filter=True,
+                meta={'item': item}
+            )
 
     def parse_abstract(self, response: scrapy.http.TextResponse):
-        # inspect_response(response, self)
         item = response.meta['item']
-        if self.new_style:
-            abstract = response.xpath(
-                '/html/body/div/div[1]/div[1]/div/article/div/div[1]/section[3]/p').get()
-            if abstract == None:
-                abstract = response.xpath(
-                    '/html/body/div/div[1]/div[1]/div/article/div/div[1]/section[3]/text()').getall()
-                abstract = ''.join(abstract)
-
-            authors = response.xpath(
-                '/html/body/div/div[1]/div[1]/div/article/div/div[1]/section[1]/ul/li/span[1]/text()').getall()
-            authors = ', '.join(authors)
-
-            file_url = response.xpath(
-                '/html/body/div/div[1]/div[1]/div/article/div/div[2]/div[2]/ul/li/a/@href').get()
-            title = response.xpath('/html/body/div/div[1]/div[1]/div/article/h1/text()').get()
-        else:
-            title = response.xpath('//*[@id="title"]/text()').get()
-            abstract = response.xpath('//*[@id="abstract"]/div').get()
-            file_url = response.xpath('//*[@id="paper"]/a/@href').get()
-            authors = response.xpath('//*[@id="author"]/em/text()').get()
+        abstract = response.xpath('//*[@id="genesis-content"]/article/div[2]/div[1]/div/p/text()').get()
+        file_url = response.xpath('//*[@id="genesis-content"]/article/div[1]/a[1]/@href').get()
 
         splitted_link = file_url.split('/')
-        if splitted_link[-3] == 'view':
-            splitted_link[-3] = 'download'
-            file_url = '/'.join(splitted_link)
-
-        pdf_url = '/'.join(splitted_link[-2:])
-        file_url += '.pdf'
-
-        if abstract == None:
-            self.logger.warning(
-                f'No abstract found for "{item["title"]}": {item["abstract_url"]}')
-            return
-
-        abstract_lines = abstract.split('\n')
-        if len(abstract_lines) > 1:
-            i = 0
-            while i < len(abstract_lines):
-                abstract_lines[i] = abstract_lines[i].strip()
-                if abstract_lines[i].endswith('-') and i < len(abstract_lines) - 1:
-                    abstract_lines[i] = f'{abstract_lines[i]}{abstract_lines.pop(i+1).strip()}'
-
-                i += 1
-
-            abstract = ' '.join(abstract_lines)
+        pdf_url = '/'.join(splitted_link[-2:])[:-4]
 
         abstract = self.clean_html_tags(abstract)
         abstract = self.clean_extra_whitespaces(abstract)
@@ -155,9 +83,7 @@ class AAAISpider(BaseSpider):
         item['abstract'] = repr(abstract)
         item['file_urls'] = [file_url]  # used to download pdf
         item['pdf_url'] = pdf_url
-        item['authors'] = authors.strip()
-
-        item['title'] = self.clean_html_tags(title)
+        item['title'] = self.clean_html_tags(item['title'])
         item['title'] = self.clean_extra_whitespaces(item['title'])
         while (item['title'].startswith('"') and item['title'].endswith('"')) or (item['title'].startswith("'") and item['title'].endswith("'")):
             item['title'] = item['title'][1:-1].strip()
