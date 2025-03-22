3da51753f03d6c0e7103dc2f9ca56a47519d6cef
George Araújo <george.gcac@gmail.com>
2025-03-22 13:25:47 -0300

    Improved html cleaning function
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- papers_scrapper/spiders/base_spider.py
+++ papers_scrapper/spiders/base_spider.py
@@ -7,29 +7,32 @@ from scrapy.spiders import CrawlSpider
 class BaseSpider(CrawlSpider):
     def __init__(self, conference: str, year: str):
         html_regex = [
-            '<b>',
-            '<strong>',
-            '<i>',
-            '<em>',
-            '<mark>',
-            '<small>',
-            '<del>',
-            '<ins>',
-            '<sub>',
-            '<sup>',
             '</b>',
-            '</strong>',
-            '</i>',
+            '</del>',
+            '</div>',
             '</em>',
+            '</i>',
+            '</ins>',
             '</mark>',
             '</small>',
-            '</del>',
-            '</ins>',
+            '</strong>',
             '</sub>',
             '</sup>',
+            '<b>',
+            '<del>',
+            '<div>',
+            '<em>',
+            '<i>',
+            '<ins>',
+            '<mark>',
+            '<small>',
+            '<strong>',
+            '<sub>',
+            '<sup>',
         ]
 
         self._html_regex = re.compile('|'.join(html_regex))
+        self._html_with_attributes_regex = re.compile(r'<[\w\s_\-\−\–=\"]+>')
         self._special_chars_regex = re.compile('[\s]*<(/)?[\w\s_\-\−\–=\"]+(/)?>[\s]*')
 
         self.conference = conference
@@ -38,6 +41,7 @@ class BaseSpider(CrawlSpider):
 
     def clean_html_tags(self, text: str) -> str:
         # remove html tags
+        text = self._html_with_attributes_regex.sub('', text)
         text = self._html_regex.sub('', text)
         return self._special_chars_regex.sub(' ', text).strip()
 
